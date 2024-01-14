9b4c5fb56934166e194bc9f4ab4073c20ae14a52
rasouli <rasouli@fastmail.com>
2024-01-14 19:03:00 +0100

    chore: improve the example code for tokio channels

--- codes/too_many_concurrent_app_with_tokio/humble_port_scanner/src/main.rs
+++ codes/too_many_concurrent_app_with_tokio/humble_port_scanner/src/main.rs
@@ -1,4 +1,5 @@
-use std::{error::Error, iter};
+use std::time::Duration;
+use std::{error::Error, future::Future, iter, net::Ipv4Addr, sync::Arc};
 
 use clap::Parser;
 
@@ -6,6 +7,10 @@ use clap::Parser;
 use ipnet::Ipv4Net;
 
 use anyhow::{anyhow, Context, Result};
+use tokio::net::TcpStream;
+use tokio::runtime::{self, Runtime};
+
+use tokio::{task, time};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -22,6 +27,19 @@ struct SubnetScanConfiguration {
     end_port: u16,
 }
 
+// *
+struct IpPortScanResult {
+    ip: Ipv4Addr,
+    port: u16,
+    state: PortState,
+}
+
+enum PortState {
+    Open,
+    Closed,
+    TimeOut,
+}
+
 fn main() {
     let PortScannerArgs { subnets, ports } = PortScannerArgs::parse();
 
@@ -35,7 +53,62 @@ fn main() {
         return;
     }
 
+    let runtime = setup_tokio_runtime();
+
     println!("all  good.");
+}
+
+async fn scan_ipv4_subnet(scan_configuration: SubnetScanConfiguration) {
+    let SubnetScanConfiguration {
+        subnet,
+        begin_port,
+        end_port,
+    } = scan_configuration;
+
+    for ip in subnet.hosts() {
+        for port in begin_port..end_port {}
+    }
+}
+
+async fn check_port_status_with_timeout(
+    ip: Ipv4Addr,
+    port: u16,
+    timeout: Duration,
+) -> IpPortScanResult {
+    let stream = time::timeout(timeout, async { TcpStream::connect((ip, port)).await }).await;
+
+    let state = match stream {
+        Err(_) => PortState::TimeOut,
+        Ok(tcp_result) if tcp_result.is_ok() => PortState::Open,
+        _ => PortState::Closed,
+    };
+
+    IpPortScanResult { ip, port, state }
+}
+
+// requires setting up the .cargo/config.toml
+async fn run_named_task<F>(task_name: String, runtime: &Arc<Runtime>, fut: F)
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    task::Builder::new()
+        .name(&task_name)
+        .spawn_on(fut, runtime.clone().handle())
+        .unwrap()
+        .await;
+}
+
+// *
+fn setup_tokio_runtime() -> Runtime {
+    runtime::Builder::new_multi_thread()
+        .worker_threads(2)
+        .thread_name("scan_runtime")
+        .enable_io()
+        .enable_time()
+        .enable_metrics_poll_count_histogram()
+        .build()
+        .expect("Failed to build Tokio Runtime.")
 }
 
 fn parse_subnet(subnet: String) -> Result<Ipv4Net> {
@@ -81,7 +154,7 @@ fn parse_port_ranges(port_range: String) -> Result<(u16, u16)> {
     Ok((begin_port, end_port))
 }
 
-fn prepare_subnets_and_ports(
+fn prepare_subnets_and_port_ranges(
     subnets: Vec<String>,
     port_ranges: Vec<String>,
 ) -> Vec<Result<SubnetScanConfiguration>> {
@@ -109,6 +182,8 @@ mod tests {
 
     use super::*;
 
+    #[tokio::test]
+    async fn check_port_status_with_time_out_tests() {}
     #[test]
     fn parse_port_ranges_test() {
         assert_eq!(
@@ -163,5 +238,14 @@ mod tests {
             Ipv4Net::new(Ipv4Addr::new(172, 16, 0, 0), 16).unwrap(),
             parse_subnet(String::from("172.16.0.0/16")).unwrap()
         )
+    }
+
+    async fn setup_test_server(host: &str, port: u16) {
+        // cont. test with a mock tcp server
+    }
+
+    #[test]
+    fn prepare_subnets_and_port_ranges_test() {
+        assert_eq!(vec![Ok(SubnetScanConfiguration {})])
     }
 }
