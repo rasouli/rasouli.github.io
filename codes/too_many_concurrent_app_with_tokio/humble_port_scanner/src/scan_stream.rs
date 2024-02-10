6512eceed4a026965b93332442c44337a2966f42
rasouli <rasouli@fastmail.com>
2024-02-10 15:56:25 +0100

    feat: improve humble scanner with a Streaming over channels

--- codes/too_many_concurrent_app_with_tokio/humble_port_scanner/src/scan_stream.rs
+++ codes/too_many_concurrent_app_with_tokio/humble_port_scanner/src/scan_stream.rs
@@ -3,16 +3,69 @@
 //   use ratatui to display : progress bar per each subnet and a scrollable table to
 //   present the scan result.
 
+use std::{future::Future, pin::Pin};
+
+use futures_core::Stream;
+use ipnet::Ipv4Subnets;
+use tokio::sync::mpsc::UnboundedReceiver;
+use tokio_stream::{StreamExt, StreamMap, StreamNotifyClose};
+
 use crate::models::IpPortScanResult;
 
-struct ScanResultStreamer {}
+struct ScanResultStreamer {
+    futures: Vec<Box<dyn Future<Output = ()>>>,
+    stream_map: Pin<
+        Box<
+            StreamMap<
+                Ipv4Subnets,
+                StreamNotifyClose<Pin<Box<dyn Stream<Item = IpPortScanResult>>>>,
+            >,
+        >,
+    >,
+}
 
-impl ScanResultStreamer {}
+impl ScanResultStreamer {
+    pub fn new(
+        scan_tasks: Vec<(
+            Ipv4Subnets,
+            Box<dyn Future<Output = ()> + 'static>,
+            UnboundedReceiver<IpPortScanResult>,
+        )>,
+    ) -> Self {
+        let mut futures: Vec<Box<dyn Future<Output = ()>>> = Vec::new();
+        let mut stream_map = StreamMap::new();
 
-impl Iterator for ScanResultStreamer {
-    type Item = IpPortScanResult;
+        for (subnet, scan_task, rx) in scan_tasks {
+            let rx_stream = StreamNotifyClose::new(Self::make_stream(rx));
 
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+            stream_map.insert(subnet, rx_stream);
+            futures.push(scan_task);
+        }
+
+        Self {
+            futures,
+            stream_map: Box::pin(stream_map),
+        }
+    }
+
+    fn make_stream(
+        mut rx: UnboundedReceiver<IpPortScanResult>,
+    ) -> Pin<Box<dyn Stream<Item = IpPortScanResult>>> {
+        Box::pin(async_stream::stream! {
+                while let Some(scan_result) = rx.recv().await {
+                    yield scan_result;
+                }
+        })
+    }
+}
+
+impl Stream for ScanResultStreamer {
+    type Item = (Ipv4Subnets, Option<IpPortScanResult>);
+
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        Stream::poll_next(self.stream_map.as_mut(), cx)
     }
 }
