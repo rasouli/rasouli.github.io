b8098515f35cf4ab465fc5bdb96ef48b4eb41453
Reza Rasouli <rasouli@fastmail.com>
2024-07-15 08:23:04 +0200

    fix: fix mute borrow panic

--- codes/morris/src/main.rs
+++ codes/morris/src/main.rs
@@ -1,7 +1,20 @@
 use std::{cell::RefCell, rc::Rc};
 
 fn main() {
-    println!("salam")
+    let mut root = BinaryTree::new(1);
+
+    let mut node_a = BinaryTree::new(7);
+    node_a.left = BinaryTree::wrap(BinaryTree::new(8));
+    node_a.right = BinaryTree::wrap(BinaryTree::new(9));
+
+    let mut root_l = BinaryTree::new(2);
+    root_l.right = BinaryTree::wrap(node_a);
+    root.left = BinaryTree::wrap(root_l);
+    root.right = BinaryTree::wrap(BinaryTree::new(3));
+
+    let tree = BinaryTree::wrap(root);
+    let traversal = traversal::morisson_inorder_traversal(&tree);
+    println!("traversal inorder result : {:?}", traversal);
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -27,9 +40,11 @@ impl BinaryTree {
 
 mod traversal {
     use crate::BinaryTree;
-    use std::borrow::BorrowMut;
-    use std::cell::RefCell;
-    use std::rc::Rc;
+    use std::{
+        borrow::{Borrow, BorrowMut},
+        cell::RefCell,
+        rc::Rc,
+    };
 
     pub fn morisson_inorder_traversal(root: &Option<Rc<RefCell<Box<BinaryTree>>>>) -> Vec<i32> {
         let mut inorder_traversal = Vec::<i32>::new();
@@ -39,20 +54,20 @@ mod traversal {
         }
 
         let mut current: Option<Rc<RefCell<Box<BinaryTree>>>> = root.clone();
+        let mut predecessor: Option<Rc<RefCell<Box<BinaryTree>>>> = None;
+        while let Some(rc_current_node) = current.clone() {
+            {
+                let current_node = rc_current_node.as_ref().borrow();
 
-        while let Some(current_rc_node) = current.clone() {
-            let current_node = current_rc_node.borrow();
+                if current_node.left.is_none() {
+                    inorder_traversal.push(current_node.value);
+                    current = current_node.right.clone();
+                    continue;
+                }
 
-            if current_node.left.is_none() {
-                inorder_traversal.push(current_node.value);
-                current = current_node.right.clone();
-                continue;
-            }
-
-            let mut predecessor: Option<Rc<RefCell<Box<BinaryTree>>>> = current_node.left.clone();
-            loop {
-                if let Some(pred_rc_node) = predecessor.clone() {
-                    let pred_node = pred_rc_node.borrow();
+                predecessor = current_node.left.clone();
+                while let Some(rc_pred_node) = predecessor.clone() {
+                    let pred_node = rc_pred_node.as_ref().borrow();
                     if pred_node.right.is_none() || pred_node.right == current {
                         break;
                     }
@@ -61,16 +76,17 @@ mod traversal {
                 }
             }
 
-            if let Some(pred_rc_node) = predecessor {
-                let mut pred_node = pred_rc_node.as_ref().borrow_mut();
-
-                if pred_node.right.is_none() {
-                    pred_node.right = current.clone();
-                    current = current_rc_node.as_ref().borrow_mut().left.clone();
-                } else {
-                    pred_node.right = None;
-                    inorder_traversal.push(current_rc_node.borrow().value);
-                    current = current_rc_node.as_ref().borrow_mut().right.clone();
+            if let Some(ref rc_pred_node) = predecessor.clone() {
+                let mut pred_node = rc_pred_node.as_ref().borrow_mut();
+                if let Some(ref rc_current_node) = current.clone() {
+                    if pred_node.right.is_none() {
+                        pred_node.right = current.clone();
+                        current = rc_current_node.as_ref().borrow().left.clone();
+                    } else {
+                        pred_node.right = None;
+                        inorder_traversal.push(rc_current_node.as_ref().borrow().value);
+                        current = rc_current_node.as_ref().borrow().right.clone();
+                    }
                 }
             }
         }
